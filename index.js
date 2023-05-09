432b78f00020ded142cd750f85e77109dfec4604
FuadAbdullah <muhammadfuadbinabdullah@gmail.com>
2023-05-09 10:39:00 +0800

    First commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const axios = require("axios");
+
+// set the URL for the API endpoint
+const url = "http://HelloWorldNVirginiaALB-2044323856.us-east-1.elb.amazonaws.com:3000/helloWorld?name=Fuad";
+
+// set the number of requests to send
+const numRequests = 2500;
+
+// send the requests
+for (let i = 0; i < numRequests; i++) {
+  axios
+    .post(url)
+    .then((response) => {
+      console.log(`Request ${i + 1} succeeded:`, response.data);
+    })
+    .catch((error) => {
+      console.error(`Request ${i + 1} failed:`, error.message);
+    });
+}
